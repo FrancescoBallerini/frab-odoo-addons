e1510ebeb2e980b004b25a24cd6ebd03ea948d78
FrancescoBallerini <francescobl.lavoro@gmail.com>
2023-12-09 18:53:39 +0100

    Fix AssertError on _convert pt2 Make check on python to avoid AssertError on convert.
    Small fixes:
    Improve flexibility by adding a context key allowing to skip min amount check.

--- website_sale_minimum_amount/controllers/main.py
+++ website_sale_minimum_amount/controllers/main.py
@@ -6,22 +6,29 @@ from odoo.http import request
 
 class WebsiteSaleMinimumAmount(WebsiteSale):
     def checkout_redirection(self, order):
+        if order:
+            # check order to avoid AssertionError in _convert()
+            # when there is no active order
+            order_amount = (
+                order.amount_untaxed
+                if request.website.website_sale_min_amount_type == "tax_excluded"
+                else order.amount_total
+            )
+            if order.currency_id and order.company_id.currency_id:
+                # avoid AssertionError in _convert() in every possible situation
+                required_amount = order.currency_id._convert(
+                    from_amount=request.website.website_sale_min_amount,
+                    to_currency=order.company_id.currency_id,
+                    company=order.company_id,
+                    date=datetime.date.today(),
+                    round=False,
+                )
+            else:
+                required_amount = request.website.website_sale_min_amount
 
-        order_amount = (
-            order.amount_untaxed
-            if request.website.website_sale_min_amount_type == "tax_excluded"
-            else order.amount_total
-        )
+            skip_check = request.env.context.get("bypass_minimum_amount_check")
 
-        required_amount = order.pricelist_id.currency_id._convert(
-            from_amount=request.website.website_sale_min_amount,
-            to_currency=order.company_id.currency_id,
-            company=order.company_id,
-            date=datetime.date.today(),
-            round=False,
-        )
-
-        if order_amount < required_amount:
-            return request.redirect("/shop/cart")
+            if not skip_check and order_amount < required_amount:
+                return request.redirect("/shop/cart")
 
         return super(WebsiteSaleMinimumAmount, self).checkout_redirection(order=order)
