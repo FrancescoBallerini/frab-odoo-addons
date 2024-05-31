45734f96e9d2457933e3715e87d0e77361d13dd2
FrancescoBallerini <francescobl.lavoro@gmail.com>
2024-06-01 01:13:40 +0200

    Implement logic in _processColumns()
    Implementing module logic in start() wasn't working properly
    in x2m listviews. By implementing it in _processColumns()
    render of optional fields will be executed in time for the
    view rendering

--- listview_make_all_fields_optional/static/src/js/list_renderer.js
+++ listview_make_all_fields_optional/static/src/js/list_renderer.js
@@ -4,18 +4,16 @@ odoo.define("listview_make_all_fields_optional.ListRenderer", function (require)
     var ListRenderer = require("web.ListRenderer");
 
     ListRenderer.include({
-        start: function () {
-            var self = this;
-            return this._super.apply(this, arguments).then(function () {
-                _.each(
-                    self.arch.children.filter((fields) => fields.tag == "field"),
-                    function (node) {
-                        if (node.attrs.optional !== "hide") {
-                            node.attrs.optional = "show";
-                        }
+        _processColumns: function (columnInvisibleFields) {
+            _.each(
+                this.arch.children.filter((fields) => fields.tag == "field"),
+                function (node) {
+                    if (node.attrs.optional !== "hide") {
+                        node.attrs.optional = "show";
                     }
-                );
-            });
+                }
+            );
+            this._super.apply(this, arguments);
         },
     });
 });
